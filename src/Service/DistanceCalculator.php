87143dc7ad5fc23bedb26308e1838f2bb00b55bd
S1N0X <antekbal06@gmail.com>
2026-08-28 01:52:00 +0200

    fix: implement correct haversine formula for calculating distance

--- src/Service/DistanceCalculator.php
+++ src/Service/DistanceCalculator.php
@@ -6,8 +6,22 @@ namespace App\Service;
 
 class DistanceCalculator
 {
+    private const EARTH_RADIUS_KM = 6371.0;
+
     public function calculate(float $lat_x, float $lon_x, float $lat_y, float $lon_y): float
     {
-        return  sqrt(pow($lat_x - $lat_y, 2) + pow($lon_x - $lon_y, 2));
+        $latFrom = deg2rad($lat_x);
+        $lonFrom = deg2rad($lon_x);
+        $latTo = deg2rad($lat_y);
+        $lonTo = deg2rad($lon_y);
+
+        $latDelta = $latTo - $latFrom;
+        $lonDelta = $lonTo - $lonFrom;
+
+        $angle = pow(sin($latDelta/2), 2) + cos($latFrom) * cos($latTo) * pow(sin($lonDelta/2), 2);
+        $centralAngle = 2 * atan2(sqrt($angle), sqrt(1-$angle));
+
+        $result = self::EARTH_RADIUS_KM * $centralAngle;
+        return round($result, 2);
     }
 }
