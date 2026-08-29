06cecbb3e514de67c53a3b14f93f536f045b79a0
S1N0X <antekbal06@gmail.com>
2026-08-29 08:38:28 +0200

    test: add integration test for city repository

---
+++ tests/Repository/CityTest.php
@@ -0,0 +1,64 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Repository;
+
+use App\Entity\City;
+use App\Repository\CityRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class CityTest extends KernelTestCase
+{
+    private EntityManagerInterface $entityManager;
+    private CityRepository $repository;
+
+    protected function setUp(): void
+    {
+        self::bootKernel();
+
+        $container = static::getContainer();
+
+        $this->entityManager = $container->get('doctrine')->getManager();
+        $this->repository = $container->get(CityRepository::class);
+
+        $this->entityManager->createQuery('DELETE FROM App\Entity\City')->execute();
+    }
+
+    public function testGetRandomCityWithPossibleStartingCities(): void
+    {
+        $this->loadTestCities();
+        $city = $this->repository->getRandomCity();
+
+        $this->assertNotNull($city);
+    }
+
+    public function testGetRandomCityWithoutPossiblieStartingCities(): void
+    {
+        $city = $this->repository->getRandomCity();
+
+        $this->assertNull($city);
+    }
+
+    private function loadTestCities(): void
+    {
+        $warsaw = new City();
+        $warsaw->setName('Warszawa');
+        $warsaw->setCountryCode('PL');
+        $warsaw->setLatitude(52.2297);
+        $warsaw->setLongitude(21.0122);
+        $warsaw->setIsStartingCity(true);
+
+        $norilsk = new City();
+        $norilsk->setName('Norilsk');
+        $norilsk->setCountryCode('RU');
+        $norilsk->setLatitude(69.3535);
+        $norilsk->setLongitude(88.2027);
+        $norilsk->setIsStartingCity(false);
+
+        $this->entityManager->persist($warsaw);
+        $this->entityManager->persist($norilsk);
+        $this->entityManager->flush();
+    }
+}
