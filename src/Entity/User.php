a67ec29d5a9e4b80e9300b52668ae6ef6ce32649
S1N0X <antekbal06@gmail.com>
2026-09-01 16:05:09 +0200

    feat: set unique consraint in user entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -13,6 +13,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`app_user`')]
+#[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
